9fddcea45eb1df262f56300982df16d7fd263cbd
amimaro <amir.zahlan@gmail.com>
2022-04-19 13:33:58 -0300

    Add a link with tecno-brega music sample

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,13 @@ export default function Index() {
   return (
     <div>
       <h1>Welcome to tecno-brega-stack</h1>
+      <a
+        href="https://www.youtube.com/watch?v=gmQ3Yp2rizM"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Just a sample of it
+      </a>
     </div>
   );
 }
