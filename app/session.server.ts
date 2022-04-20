82003bbee5de59adc37a5bfff91a835a29302ca5
amimaro <amir.zahlan@gmail.com>
2022-04-20 19:55:46 -0300

    Check for SERVER_URL

--- app/session.server.ts
+++ app/session.server.ts
@@ -4,6 +4,10 @@ if (!process.env.SESSION_SECRET) {
   throw Error("SESSION_SECRET must be set");
 }
 
+if (!process.env.SERVER_URL) {
+  throw Error("SERVER_URL must be set");
+}
+
 export const sessionKey = process.env.SESSION_KEY || "__supabase_session";
 
 export const { getSession, commitSession, destroySession } =
