7196ab8dfe1a533f62193b0c48da31923b5f7a00
amimaro <amir.zahlan@gmail.com>
2022-04-21 15:26:41 -0300

    Allow deploy to vercel

--- remix.config.js
+++ remix.config.js
@@ -2,12 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  // Uncomment these two options below to deploy to vercel
+  // Comment these two options below to deploy to fly
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  // server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  // serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  serverBuildTarget: "vercel",
 
   ignoredRouteFiles: [".*"],
   // appDirectory: "app",
