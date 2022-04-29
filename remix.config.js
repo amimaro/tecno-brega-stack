81f2c6b7ca51927e1238afd8f0859df42edf496a
amimaro <amir.zahlan@gmail.com>
2022-04-29 11:09:46 -0300

    Update packages

--- remix.config.js
+++ remix.config.js
@@ -3,8 +3,4 @@
  */
 module.exports = {
   ignoredRouteFiles: [".*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
-  // publicPath: "/build/",
 };
