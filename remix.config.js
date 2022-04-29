dda6a9a5a1c840356d6556933157a6e397303c22
amimaro <amir.zahlan@gmail.com>
2022-04-29 10:54:57 -0300

    Remove vercel server

--- remix.config.js
+++ remix.config.js
@@ -2,13 +2,6 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  // Comment these two options below to deploy to fly
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  serverBuildTarget: "vercel",
-
   ignoredRouteFiles: [".*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
